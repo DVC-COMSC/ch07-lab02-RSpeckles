c4df9236b91ebb982047c7b370bb219ea60ba4fe
RSpeckles <rchenspeckman@gmail.com>
2022-10-04 12:11:29 -0700

    Completed lab

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 # ******************************
 # Make your Code
 # ******************************
+import random
+
+numbers = []
+
+while len(numbers) < 10:
+    numbers.append(random.randint(0,100))
+
+
+smallest = min(numbers)
+print(smallest)
+print(numbers.index(smallest))
